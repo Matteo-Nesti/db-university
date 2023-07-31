eb45d7e1402ca44672ac922dc520c8f0e2b4a810
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-07-31 15:04:10 +0200

    join start

--- file.sql
+++ file.sql
@@ -8,11 +8,11 @@ SELECT * FROM `courses` WHERE `cfu` > 10;
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
 
-SELECT * FROM `students` WHERE (2023 - YEAR(`date_of_birth`)) > 30;
+SELECT * FROM `students` WHERE `date_of_birth` <= DATE_SUB(CURDATE(), INTERVAL 30 YEAR);
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
-SELECT * FROM `courses` WHERE `period` LIKE 'I %' AND `year` = 1;
+SELECT * FROM `courses` WHERE `period` = 'I semestre' AND `year` = 1;
 
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
@@ -46,3 +46,27 @@ SELECT `student_id` AS `ID Studente`, ROUND(AVG(`vote`)) AS `Media` FROM `exam_s
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
 
 SELECT COUNT(*) AS `Corsi Totali`, `department_id` AS `Dipartimento` FROM `degrees` GROUP BY `department_id`;
+
+
+
+----------------------------------------------------------------------------
+
+
+
+-- Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+
+-- Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+
+-- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+
+
+-- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+
+
+-- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+
+
+-- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+
+
+-- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
